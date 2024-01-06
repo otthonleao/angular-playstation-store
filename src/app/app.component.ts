02a84ffa25c27393ae08eb400ae7cee0a8f0352d
Otthon Leão <otthonleao@hotmail.com>
2024-01-06 00:58:59 -0400

    feat: add components, pages/home and style css default

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MenuBarComponent } from "./components/menu-bar/menu-bar.component";
+import { HomeComponent } from "./pages/home/home.component";
+import { CardComponent } from './components/card/card.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet, MenuBarComponent, HomeComponent, CardComponent]
 })
 export class AppComponent {
   title = 'playstation-store';
