b281223c15804dfeee56b63fded42d96a018d0c0
Otthon Leão <otthonleao@hotmail.com>
2024-01-06 16:18:13 -0400

    chore(card): add components in atomos level for card

---
+++ src/app/components/card/card-label/card-label.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-label',
+  standalone: true,
+  imports: [],
+  templateUrl: './card-label.component.html',
+  styleUrl: './card-label.component.css'
+})
+export class CardLabelComponent {
+
+}
